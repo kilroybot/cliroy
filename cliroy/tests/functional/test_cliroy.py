01f7ee8aed51f52980f7951ef3ab337058ef2589
Sebastian Pietras <01133337@pw.edu.pl>
2022-06-30 19:21:43 +0200

    Updated files to match newer cookiecutter version

--- cliroy/tests/functional/test_cliroy.py
+++ cliroy/tests/functional/test_cliroy.py
@@ -18,20 +18,6 @@ class TestCliroy:
     def runner(self):
         return CliRunner()
 
-    def test_cliroy_returns_zero(self, runner):
-        result = runner.invoke(cli)
-        assert not result.exception
-        assert result.exit_code == 0
-
-    def test_cliroy_prints_something(self, runner):
-        result = runner.invoke(cli)
-        assert result.output
-
     def test_cliroy_prints_help(self, runner):
-        result = runner.invoke(cli, ['--help'])
+        result = runner.invoke(cli, ["--help"])
         assert "Usage" in result.output
-
-    def test_cliroy_works_with_arg(self, runner):
-        result = runner.invoke(cli, ['--x', 2])
-        assert not result.exception
-        assert result.exit_code == 0
